158f5cfaf6d003ebd523876586b70bcc559c78b3
yankun <kun.yan9@newtouc.com>
2026-06-26 18:41:15 +0800

    Adjust user management columns

--- web/classic/src/components/table/users/UsersColumnDefs.jsx
+++ web/classic/src/components/table/users/UsersColumnDefs.jsx
@@ -178,29 +178,6 @@ const renderQuotaUsage = (text, record, t) => {
 };
 
 /**
- * Render invite information
- */
-const renderInviteInfo = (text, record, t) => {
-  return (
-    <div>
-      <Space spacing={1}>
-        <Tag color='white' shape='circle' className='!text-xs'>
-          {t('邀请')}: {renderNumber(record.aff_count)}
-        </Tag>
-        <Tag color='white' shape='circle' className='!text-xs'>
-          {t('收益')}: {renderQuota(record.aff_history_quota)}
-        </Tag>
-        <Tag color='white' shape='circle' className='!text-xs'>
-          {record.inviter_id === 0
-            ? t('无邀请人')
-            : `${t('邀请人')}: ${record.inviter_id}`}
-        </Tag>
-      </Space>
-    </div>
-  );
-};
-
-/**
  * Render operations column
  */
 const renderOperations = (
@@ -328,6 +305,15 @@ export const getUsersColumns = ({
       render: (text, record) => renderUsername(text, record),
     },
     {
+      title: t('邮箱'),
+      dataIndex: 'email',
+      render: (text) => (
+        <span style={{ fontSize: 12, color: 'var(--semi-color-text-2)' }}>
+          {text || '-'}
+        </span>
+      ),
+    },
+    {
       title: t('状态'),
       dataIndex: 'info',
       render: (text, record, index) =>
@@ -351,11 +337,6 @@ export const getUsersColumns = ({
       render: (text, record, index) => {
         return <div>{renderRole(text, t)}</div>;
       },
-    },
-    {
-      title: t('邀请信息'),
-      dataIndex: 'invite',
-      render: (text, record, index) => renderInviteInfo(text, record, t),
     },
     {
       title: t('创建时间'),
